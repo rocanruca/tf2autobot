80d243fbed7327336483bc2b9647a2dd4543b805
idinium96 <idin100msc@gmail.com>
2020-08-09 07:55:09 +0800

    fix something

--- src/classes/MyHandler.ts
+++ src/classes/MyHandler.ts
@@ -1790,19 +1790,28 @@ export = class MyHandler extends Handler {
 
         if ((isBankingBuyKeysWithEnoughRefs && isEnableKeyBanking) || isBuyingKeys) {
             if (amountKeysCanBuy <= 1) {
-                setMinKeys = currKeys;
-                setMaxKeys = currKeys + 1;
+                setMinKeys = currKeys <= userMinKeys ? userMinKeys : currKeys;
+                setMaxKeys = currKeys + 1 >= userMaxKeys ? userMaxKeys : currKeys + 1;
             } else {
-                setMinKeys = currKeys;
-                setMaxKeys = currKeys + 1 + truncedAmountKeysCanBuy;
+                setMinKeys = currKeys <= userMinKeys ? userMinKeys : currKeys;
+                setMaxKeys =
+                    currKeys + 1 + truncedAmountKeysCanBuy >= userMaxKeys
+                        ? userMaxKeys
+                        : currKeys + 1 + truncedAmountKeysCanBuy;
             }
         } else if (isSellingKeys) {
             if (amountKeysCanSell <= 1) {
-                setMinKeys = currKeys - 1;
-                setMaxKeys = userMinKeys;
+                setMinKeys = currKeys - 1 <= userMinKeys ? userMinKeys : currKeys - 1;
+                setMaxKeys = currKeys >= userMaxKeys ? userMaxKeys : currKeys;
             } else {
-                setMinKeys = currKeys - 1 - truncedAmountKeysCanSell;
-                setMaxKeys = userMinKeys;
+                setMinKeys =
+                    currKeys - 1 - truncedAmountKeysCanSell <= userMinKeys
+                        ? userMinKeys
+                        : currKeys - 1 - truncedAmountKeysCanSell;
+                setMaxKeys =
+                    currKeys + 1 + truncedAmountKeysCanBuy >= userMaxKeys
+                        ? userMaxKeys
+                        : currKeys + 1 + truncedAmountKeysCanBuy;
             }
         } else if (isBankingKeys) {
             if (amountKeysCanBank <= 1) {
@@ -1818,6 +1827,10 @@ export = class MyHandler extends Handler {
                         ? userMaxKeys
                         : currKeys + 1 + truncedAmountKeysCanBank;
             }
+        }
+
+        if ((isBankingKeys && isEnableKeyBanking) || isBuyingKeys || isSellingKeys) {
+            log.debug(`Min: ${setMinKeys}, Max: ${setMaxKeys}`);
         }
 
         const isAlreadyRunningAutokeys = this.autokeysIsActive !== false;

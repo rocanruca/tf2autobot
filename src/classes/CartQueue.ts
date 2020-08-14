4d7d22eaa4388d8e0b71f2d63c661be916190c4f
idinium96 <idin100msc@gmail.com>
2020-08-15 01:16:02 +0800

    update CartQueue.ts

--- src/classes/CartQueue.ts
+++ src/classes/CartQueue.ts
@@ -57,14 +57,14 @@ class CartQueue {
         log.debug(`Checking queue position in 3 minutes...`);
         this.queuePositionCheck = setTimeout(() => {
             const position = this.carts.length;
-            log.debug(`Current queue position: ${position + 1}`);
+            log.debug(`Current queue position: ${position}`);
             if (position >= 2) {
                 if (
                     process.env.DISABLE_DISCORD_WEBHOOK_SOMETHING_WRONG_ALERT === 'false' &&
                     process.env.DISCORD_WEBHOOK_SOMETHING_WRONG_ALERT_URL
                 ) {
                     const time = (this.bot.handler as MyHandler).timeWithEmoji();
-                    this.discord.sendQueueAlert(position + 1, time.time);
+                    this.discord.sendQueueAlert(position, time.time);
                     this.bot.botManager
                         .restartProcess()
                         .then(restarting => {
@@ -79,7 +79,7 @@ class CartQueue {
                             this.discord.sendQueueAlertFailedError(err.message, time.time);
                         });
                 } else {
-                    this.bot.messageAdmins(`⚠️ [Queue alert] Current position: ${position + 1}`, []);
+                    this.bot.messageAdmins(`⚠️ [Queue alert] Current position: ${position}`, []);
                     this.bot.botManager
                         .restartProcess()
                         .then(restarting => {
